b0c795554af9b776a0756bf7dad79e191fab53dc
KlajdiMexhaj <mexhjklajdi@gmail.com>
2023-09-14 11:13:35 +0200

    add models

--- appDjango/views.py
+++ appDjango/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 def home(request):
+    kategori = Kategori.objects.all()
     items = Item.objects.all()
-    context = {"Items_value":items}
+    context = {"Items_value":items,"Kategori_data":kategori}
     return render(request,'home.html',context)
 
 def about(request):
-    return render(request,'about.html')
+    staffs = Staff.objects.all()
+    context = {"Staff_data":staffs}
+    return render(request,'about.html',context)
 
 def contact(request):
     return render(request,'contact.html')
